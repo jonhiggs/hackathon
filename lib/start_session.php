42f96b9229f8905a41d9252711890c91210eb9ed
Jon Higgs <jhiggs0@gmail.com>
2013-01-25 15:59:46 +0100

    parse back the pokemon details.

--- lib/start_session.php
+++ lib/start_session.php
@@ -7,7 +7,7 @@ function user_exists($id) {
   $response = $database->query("SELECT count(*) FROM users WHERE md5='$id'");
   if ($response->fetchColumn() > 0) {
     return true;
-  }else {
+  } else {
     return false;
   }
 }
@@ -17,6 +17,29 @@ function create_user($id) {
   $query = $database->prepare("INSERT INTO users (md5) VALUES (:id);");
   $query->bindParam(':id', $id);
   $query->execute();
+}
+
+
+function get_pokemon_type($pokemon_id) {
+  $query = $database->prepare("SELECT (type) FROM pokemon WHERE id=:pokemon_id");
+  $query->bindParam(':type', $pokemon_id);
+  $query->execute();
+  $result = $query->fetchAll();
+
+  foreach ($result as $data) {
+    return $data["type"];
+  }
+}
+
+function get_pokemon_image_path($pokemon_id) {
+  $query = $database->prepare("SELECT (image_path) FROM pokemon WHERE id=:pokemon_id");
+  $query->bindParam(':id', $pokemon_id);
+  $query->execute();
+  $result = $query->fetchAll();
+
+  foreach ($result as $data) {
+    return $data["image_path"];
+  }
 }
 
 function get_pokemons($id) {
@@ -39,4 +62,11 @@ if ( ! user_exists($user_id) ) {
 
 ?>
 
-{'user_token': '<?php $user_id ?>', 'pokemon': '<?php echo get_pokemons($user_id) ?>'}
+{'user_token': '<?php $user_id ?>',
+'pokemon:' <?php
+  foreach ( get_pokemons($user_id) as $pokemon_id ) {
+    echo "'type:' '". get_pokemon_type($id) . "'";
+    echo "'front_image:' '". get_pokemon_path($id) . "'";
+    echo "'back_image:' 'b". get_pokemon_path($id) . "'";
+  }?>
+}
